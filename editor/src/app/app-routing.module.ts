2920ad151fb14a99306d1ee3edc83228a63bfcc6
anonysoul <105715150+anonysoul@users.noreply.github.com>
2023-03-15 10:39:49 +0800

    chore: upgrade angular version to 15 in editor (#6)

--- editor/src/app/app-routing.module.ts
+++ editor/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EditorComponent } from './editor/editor.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EditorComponent } from "./editor/editor.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/editor" },
